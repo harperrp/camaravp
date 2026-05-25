8eed96de43d8605b128c4d830890d44629d9d08f
harperrp <124433217+harperrp@users.noreply.github.com>
2026-05-25 01:11:40 -0300

    Add MySQL API config placeholder

---
+++ api/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'db_host' => 'localhost',
+  'db_port' => '3306',
+  'db_name' => 'camaravgp',
+  'db_user' => 'usercamara',
+  'db_pass' => 'ALTERAR_NO_SERVIDOR',
+  'session_name' => 'camaravp_admin',
+];
